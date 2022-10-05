2b4337373936fda8ebd4e961297c60c0089ebdea
Bruna Teles <43624494+brunatbc@users.noreply.github.com>
2022-10-05 17:21:29 -0300

    dom

---
+++ seção-5-DOM/script.js
@@ -0,0 +1,7 @@
+(function(){
+
+    const nomeUsuario = "Meu Nome"
+    //const tela = document.querySelector(".top-bar p");
+    //document.querySelector(".top-bar p").textContent = "Boas Vindas, "+nomeUsuario;
+    //tela.textContent += `${nomeUsuario}!`;
+}) ()
