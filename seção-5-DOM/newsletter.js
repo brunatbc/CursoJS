15f8fc6f7010fc582393dbf141ea82e12fe2942f
Bruna Teles <43624494+brunatbc@users.noreply.github.com>
2022-10-11 17:42:49 -0300

    inputs

--- seção-5-DOM/newsletter.js
+++ seção-5-DOM/newsletter.js
@@ -1,13 +1,20 @@
-(function(){
+    /*const txtEmail = document.getElementById("txtEmail");
+    const msgFeedback = document.getElementById("newsletterFeedback")
+    
+    
+    function cadastrarEmail(){
+        let email = txtEmail.value; // Se essa declaração estiver fora da função, o valor de retorno será vazio, o trecho não retornará nada por ser renderizado antes do que deveria. 
+        msgFeedback.innerHTML= `O email ${email} foi cadastrado com sucesso!`
+    }*/
+
 
     const txtEmail = document.getElementById("txtEmail");
-    
+    function editarEmail(){
+
+    }
 
 
 
 
 
 
-
-
-})()
